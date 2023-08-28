93b67cecc31f4ac7c95de5689db01aa8e5382b55
JuChan Jeong <jjc6252498@gmail.com>
2023-08-28 11:28:40 +0900

    캐릭터, 아이템 파일 분리

---
+++ TextRPG_Team/Skill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_Team
+{
+    public class Skill
+    {
+        // 정보 MP 추가
+        // 캐릭터에서 가져오기
+    }
+}
